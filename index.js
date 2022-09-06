cdf00e41fbb9fc5c0dc48f800931d1c0a6c88c99
Madeleine Heineman <mheineman16@gmail.com>
2022-09-06 13:00:12 -0400

    commit tree e476513692be87f6cc5df460cb35e42216c0602b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution here
+//existing array = drivers
+// takes array of names and a string as arguements to return list of drivers
+
+// function findMatching(array, string) {
+//     return drivers === string
+// }
+
+// console.log(findMatching(drivers, "Bobby"))
+const copies = drivers.filter(function findMatching(drivers, string) {
+    return drivers === string
+  })
